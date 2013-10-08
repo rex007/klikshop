63e3c460c458fbc2736c7a1c530c9ed49972d97d
Andraz Kuret <rex007@gmail.com>
2013-10-08 18:11:05 +0200

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Klikshop::Application.config.secret_key_base = '4b84b35d662a3cbb82a9d9e43d28c18aa6810f377a1e8667f52143b52e3ad4ab172ca70fc9fb293127ffb1401ac49c13dfb4be6cf9d5147521b826298eeefaa3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Klikshop::Application.config.session_store :cookie_store, key: '_klikshop_session'
